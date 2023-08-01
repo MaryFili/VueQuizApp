4e2ba571b50245b92ea6a98ead013b42f35accdb
MaryFili <filardimary@gmail.com>
2023-08-01 12:02:08 +0200

    card component and props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router';
 import q from './data/quizes.json';
-import {ref} from "vue";
+import {ref, watch} from "vue";
+import Card from './components/Card.vue';
 
 const quizes = ref(q);
+const search =ref("");
+
+
+watch(search, () =>{
+  quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()));
+});
 
 </script>
 
@@ -11,30 +18,33 @@ const quizes = ref(q);
 
 
 <template>
-<div>
+<div class="main-container">
       <header>
         <h1>Quizes</h1>
         <input v-model.trim="search" type="text" placeholder="Search...">
       </header>
       <div class="options-container">
-        <div v-for="quiz in quizes" :key="quiz.id" class="card">
-          <img :src="quiz.img" alt="">
-          <div class="card-text">
-            <h2>{{quiz.name}}</h2>
-            <p>{{quiz.questions.length}} questions</p>
-          </div>
-        </div>
-
+          <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
       </div>
     </div>
 </template>
 
 <style scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 80%
+}
+
   header {
       margin-bottom: 10px;
       margin-top: 30px;
       display: flex;
       align-items: center;
+   
     }
   
     header h1 {
@@ -55,29 +65,4 @@ const quizes = ref(q);
       margin-top: 40px;
     }
 
-    /* CARD */
-
-    .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-  }
 </style>
